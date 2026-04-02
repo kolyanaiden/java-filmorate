91264f29d28b264a6510bdf0bc6502a5f56bf59a
Kolya Naiden <kolyanaiden@yandex.ru>
2026-04-02 19:00:42 +0300

    Правил стиль

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,10 +6,17 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
+
     Film addFilm(Film film);
+
     Film updateFilm(Film film);
+
     void deleteFilm(Integer id);
+
     Optional<Film> getFilmById(Integer id);
+
     List<Film> getAllFilms();
+
     boolean existsById(Integer id);
+
 }
